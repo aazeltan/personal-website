65ae4d78644ccafe12df99c3f44a171aefd9a003
aazel tan <aazeltan@gmail.com>
2024-10-31 16:58:58 -0700

    changed information and edited intro

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import VueSmoothScroll from 'vue3-smooth-scroll'
-
-const app = createApp(App)
-app.use(VueSmoothScroll)
-app.mount('#app')
